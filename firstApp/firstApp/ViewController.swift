08a1a0a535aa03147476db95711de777634b7bde
Артем Закиров <>
2018-10-09 11:50:30 +0300

    hmm

---
+++ firstApp/firstApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  firstApp
+//
+//  Created by Артем Закиров on 08.10.2018.
+//  Copyright © 2018 CasCorp. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
