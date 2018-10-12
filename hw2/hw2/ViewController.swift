b55b265819f70af82c0fd2c3fef5bf8cc9a721e2
Артем Закиров <>
2018-10-12 16:19:43 +0300

    started hw2

---
+++ hw2/hw2/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  hw2
+//
+//  Created by Артем Закиров on 12.10.2018.
+//  Copyright © 2018 CasCorp. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    @IBAction func close(_ sender: Any) {
+    }
+    
+}
+
