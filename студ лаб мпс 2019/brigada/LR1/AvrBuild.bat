a3c9635dc9970d421813a424c8b3d33ff46aab5e
casperancho <artemzakirov1998@gmail.com>
2019-02-18 21:30:33 +0300

    nice

--- студ лаб мпс 2019/brigada/LR1/AvrBuild.bat
+++
@@ -1,2 +0,0 @@
-@ECHO OFF
-"C:\Program Files (x86)\Atmel\AVR Tools\AvrAssembler2\avrasm2.exe" -S "C:\brigada\LR1\labels.tmp" -fI -W+ie -C V2E -o "C:\brigada\LR1\LR1.hex" -d "C:\brigada\LR1\LR1.obj" -e "C:\brigada\LR1\LR1.eep" -m "C:\brigada\LR1\LR1.map" "C:\brigada\LR1\LR1.asm"
